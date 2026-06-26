9d013786fc941c86f6ce74253105417a5331d812
Abdul Basith <basithnoor@gmail.com>
2026-06-26 15:34:41 +0530

    Parallel Lives v1.0 Scene 01 + Scene 02 locked Cinematic language established

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Parallel Lives");
+const scene2 = document.querySelector('.scene-02');
+const ambient = document.querySelector('.ambient-light');
+
+window.addEventListener('scroll', () => {
+
+    const trigger = scene2.offsetTop;
+    const current = window.scrollY;
+
+    const progress = Math.min(
+        Math.max(
+            (current - trigger + window.innerHeight) /
+            window.innerHeight,
+            0
+        ),
+        1
+    );
+
+    document.body.style.backgroundColor = `rgb(
+        ${5 + progress * 210},
+        ${5 + progress * 205},
+        ${5 + progress * 190}
+    )`;
+
+    ambient.style.opacity = progress * 0.5;
+
+});
